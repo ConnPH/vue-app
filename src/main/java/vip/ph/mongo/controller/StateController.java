954308c9a48a0f9f4dbc170a7f5ab04d95a49c7c
root <root@163.com>
2021-07-15 11:46:57 +0800

    添加工具类 json md5 雪花算法

--- src/main/java/vip/ph/mongo/controller/StateController.java
+++ src/main/java/vip/ph/mongo/controller/StateController.java
@@ -1,7 +1,6 @@
 package vip.ph.mongo.controller;
 
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
-import io.swagger.models.auth.In;
 import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
@@ -9,7 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-import vip.ph.mongo.pojo.Result;
+import vip.ph.mongo.pojo.R;
 import vip.ph.mongo.pojo.State;
 import vip.ph.mongo.pojo.StateDetail;
 import vip.ph.mongo.service.StateDetailService;
@@ -17,10 +16,7 @@ import vip.ph.mongo.service.StateService;
 import vip.ph.mongo.vo.StateVo;
 
 import javax.annotation.Resource;
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 /**
  * @author Just be alive
@@ -78,7 +74,7 @@ public class StateController {
      */
     @GetMapping("/getStateDetail")
     @ResponseBody
-    public Result getStateDetail(Integer stateId){
+    public R getStateDetail(Integer stateId){
         State state = stateService.getById(stateId);
 
         QueryWrapper<StateDetail> stateDetailQueryWrapper = new QueryWrapper<>();
@@ -87,7 +83,7 @@ public class StateController {
         StateVo stateVo = new StateVo();
         BeanUtils.copyProperties(state,stateVo);
         stateVo.setStateDetailList(stateDetailList);
-        return Result.ok();
+        return R.ok().data("stateVo",stateVo);
     }
 
 
